e2fff0b732874730299f4b9e40225da130a3b6e8
Daniel Culley <daniel_cornwall@hotmail.com>
2021-01-10 15:34:15 +0000

    better homepage latout

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -22,3 +22,19 @@ $(document).ready(function(){
     $(".gitcard").toggleClass("backflip");
   });
 });
+
+//expanding cards
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach((panel) => {
+  panel.addEventListener('click', () =>{
+    removeActiveClasses()
+    panel.classList.add('active')
+  })
+})
+
+function removeActiveClasses(){
+  panels.forEach(panel =>{
+      panel.classList.remove('active')
+  })
+}
