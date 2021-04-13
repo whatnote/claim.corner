4c423e60494521c2c622c2f22655d375c373503d
Daniel Culley <daniel_cornwall@hotmail.com>
2021-04-13 01:46:08 +0100

    insurer contacts added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -22,19 +22,3 @@ $(document).ready(function(){
     $(".gitcard").toggleClass("backflip");
   });
 });
-
-//expanding cards
-const panels = document.querySelectorAll('.panel')
-
-panels.forEach((panel) => {
-  panel.addEventListener('click', () =>{
-    removeActiveClasses()
-    panel.classList.add('active')
-  })
-})
-
-function removeActiveClasses(){
-  panels.forEach(panel =>{
-      panel.classList.remove('active')
-  })
-}
